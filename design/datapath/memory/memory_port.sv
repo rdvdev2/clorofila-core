514af61a0a09e7a917202f76abb77108e45c75dc
Nara Díaz Viñolas <rdvdev2@gmail.com>
2024-10-14 20:15:53 +0200

    Fix previous commit (oopsie)

--- design/datapath/memory/memory_port.sv
+++ design/datapath/memory/memory_port.sv
@@ -7,6 +7,9 @@ interface memory_port;
 	logic we;
 	logic [`ADDR_MASK] addr;
 	memory_access_width_t width;
-	logic [`WORD_MASK] mem_rd;
-	logic [`WORD_MASK] mem_wr;
+	logic [`WORD_MASK] data_wr;
+	logic [`WORD_MASK] data_rd;
+
+	modport memory (input valid, input we, input addr, input width, input data_wr, output data_rd);
+	modport datapath (output valid, output we, output addr, output width, output data_wr, input data_rd);
 endinterface;
